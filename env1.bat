47851936a0b43749db80a45655411aebcb9d15dc
bhartikukreja2015 <bhartikukreja2015@gmail.com>
2015-10-30 23:04:53 -0700

    Create env1.bat

---
+++ env1.bat
@@ -0,0 +1,9 @@
+set BEA_HOME=C:\Oracle\Middleware\Oracle_Home
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_79
+set JAVA_OPTS=-Xms512m -Xmx1024m -XX:MaxPermSize=256m
+set M2_HOME=C:\Users\Administrator\Desktop\apache-maven-3.3.3
+set M2=%M2_HOME%\bin
+set MAVEN_OPTS=-Xms512m
+set PATH=%PATH%;%JAVA_HOME%\bin;%M2%;
+set WL_HOME=%BEA_HOME%\wlserver
+set WEBLOGIC_DIR=%WL_HOME%\server
